402f91f8aba325038f4192c639348993555f96d3
dldud <dldudsh83@gmail.com>
2021-11-23 18:23:13 +0900

    11_23 Grobal Exception1

--- src/main/java/com/fastcam/programing/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcam/programing/dmaker/controller/DMakerController.java
@@ -70,18 +70,4 @@ public class DMakerController {
         return dMakerService.deleteDeveloper(memberId);
     }
 
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    @ExceptionHandler(DMakerException.class)
-    public DMakerErrorResponse handleException (
-            DMakerException e,
-            HttpServletRequest request
-    ){
-        log.error("errorCode:{}, url: {}, message: {}",
-                e.getDMakerErrorCode(), request.getRequestURI(), e.getDetailMessage());
-
-        return DMakerErrorResponse.builder()
-                .errorCode(e.getDMakerErrorCode())
-                .errorMessage((e.getMessage()))
-                .build();
-    }
 }
